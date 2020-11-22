a7d912eedd88db05debcf02d0d3f2b18db2b4da4
Melkomukov Kirill <lw1korsar@gmail.com>
2020-11-22 17:38:26 +0300

    2nd rev

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, unique=True)
@@ -18,6 +19,12 @@ class Post(models.Model):
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name="posts")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE,
-                              related_name="group", blank=True,
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL,
+                              related_name="posts", blank=True,
                               null=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ["-pub_date"]
